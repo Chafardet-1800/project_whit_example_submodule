adbd11a7670a709894dab7152fada7ddc2f0b19d
Chafardet-1800 <chafargetanibal@gmail.com>
2024-01-03 09:57:37 -0400

    Eliminacion de commun entre los archivos del repositorio

--- src/app/commun/src/app/environments/constants.ts
+++
@@ -1,2 +0,0 @@
-export const CC_NODE_ENV = 'Developer';
-export const CC_FTEMPLATE_GATEWAY = 'http://172.16.4.100:4001'
--- src/app/commun/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { CmmModule } from './app/commun.module';
-
-
-platformBrowserDynamic().bootstrapModule(CmmModule)
-  .catch(err => console.error(err));
